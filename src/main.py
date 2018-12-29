c1ab50f11cf23ef92f15009ba358437600799057
gsvic <victorasgs@gmail.com>
2018-12-29 22:32:03 +0200

    minor refactorings; documentation added

--- src/main.py
+++ src/main.py
@@ -2,18 +2,20 @@ import pandas as pd
 
 from tasks import WordCloudTask
 
-test_set = pd.read_csv("../datasets/test_set.csv", sep="\t")
-train_set = pd.read_csv("../datasets/train_set.csv", sep="\t")
 
+class Main:
+  test_set = pd.read_csv("../datasets/test_set.csv", sep="\t")
+  train_set = pd.read_csv("../datasets/train_set.csv", sep="\t")
 
-def wordcloud(train, test):
-  wc = WordCloudTask(train, test)
-  wc.run()
+  @staticmethod
+  def wordcloud(train, test):
+    wc = WordCloudTask(train, test)
+    wc.run()
 
+  @staticmethod
+  def duplicates(train, test):
+    pass
 
-def duplicates(train, test):
-  pass
-
-
-def classification(train, test):
-  pass
+  @staticmethod
+  def classification(train, test):
+    pass
--- src/tasks/task.py
+++ src/tasks/task.py
@@ -3,12 +3,27 @@ from abc import abstractmethod
 
 class Task:
     def __init__(self, train, test):
+        """
+        Task constructor
+        :param train: The training dataset
+        :param test: The test dataset
+        """
         self.train = train
         self.test = test
 
     @abstractmethod
     def run_task(self):
+        """
+        The main run_task method. This abstract method should be implemented in
+        any Task sub-class implementation.
+        :return: The result (if any)
+        """
         pass
 
     def run(self):
-        self.run_task()
+        """
+        This is a generic method that is invoked in order to trigger
+        the Task execution
+        :return: Whatever run_task returns
+        """
+        return self.run_task()
