7081b84ea427ff4a8947842e0775ff8d3c31e7cc
Dimitris <dcvogi@gmail.com>
2018-12-29 11:49:05 +0200

    Structure

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+test_set = pd.read_csv("../datasets/test_set.csv", sep="\t")
+train_set = pd.read_csv("../datasets/train_set.csv", sep="\t")
+
+def wordcloud(train, test):
+
+  return
+
+def duplicates(train, test):
+
+  return
+
+def classification(train, test):
+
+  return
+
