c8a5ca6803dbbb8d1ef18e82c7ac0373945cb5fc
gsvic <victorasgs@gmail.com>
2018-12-29 22:23:25 +0200

    minor refactorings

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
+from tasks import WordCloudTask
+
 test_set = pd.read_csv("../datasets/test_set.csv", sep="\t")
 train_set = pd.read_csv("../datasets/train_set.csv", sep="\t")
 
-def wordcloud(train, test):
 
-  return
+def wordcloud(train, test):
+  wc = WordCloudTask(train, test)
+  wc.run()
+
 
 def duplicates(train, test):
+  pass
 
-  return
 
 def classification(train, test):
-
-  return
-
+  pass
